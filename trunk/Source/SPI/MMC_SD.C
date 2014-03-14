0097946921403f14caf3be727c7d6b7ebc22fa1e
pthelong@gmail.com <pthelong@gmail.com@5c9c312e-5ef8-758d-1564-8314ce06484d>
2014-03-14 06:04:34 +0000

    Refactoring sd card project

--- trunk/Source/SPI/MMC_SD.C
+++ trunk/Source/SPI/MMC_SD.C
@@ -1,10 +1,9 @@
 #include "sys.h"
-#include "M45PE161.H"
+#include "SPI.H"
 #include "mmc_sd.h"
 #include "lpc17xx.h"
-#include "ssp0.h"
 #include "../fatfs/diskio.h"
-u8  SD_Type=0;				//SD卡的类型
+u8  SD_Type=0;				//SD card type
 
 /* RTC timer strcuture */
 typedef struct {
@@ -214,12 +213,6 @@ DWORD get_fattime ()
 //	if (n) Timer2 = --n;   				
 //}
 
-/************************************************************************																										
-** 函数功能: 等待SD卡回应	
-** 入口参数: Response:要得到的回应值	    		
-** 出口参数: 返回值:0,成功得到了该回应值,其他回应值失败		
-** 备    注: 无
-************************************************************************/
 u8 SD_GetResponse(u8 Response)
 {
 	u16 Count=0xFFF;//等待次数	   						  
@@ -227,12 +220,7 @@ u8 SD_GetResponse(u8 Response)
 	if (Count==0)return MSD_RESPONSE_FAILURE;//得到回应失败   
 	else return MSD_RESPONSE_NO_ERROR;//正确回应
 }
-/************************************************************************																										
-** 函数功能: 等待SD卡写入完成	
-** 入口参数: 	    		
-** 出口参数: 返回值:0,成功得到了该回应值,其他回应值失败		
-** 备    注: 无
-************************************************************************/
+
 u8 SD_WaitDataReady(void)
 {
     u8 r1=MSD_DATA_OTHER_ERROR;
@@ -268,12 +256,11 @@ u8 SD_WaitDataReady(void)
 }
 
 /************************************************************************																										
-** 函数功能: 向SD卡发送一个命令	
-** 入口参数: u8 cmd   命令 
-**			 u32 arg  命令参数
-**	    	 u8 crc   crc校验值	
-** 出口参数: SD卡返回的响应			
-** 备    注: 无
+** Description: Send command to sdcard
+** Input: u8 cmd   Command code 
+**			 u32 arg 
+**	    	 u8 crc   Check sum
+** Ouput: return value
 ************************************************************************/													  
 u8 SD_SendCommand(u8 cmd, u32 arg, u8 crc)
 {
@@ -305,14 +292,6 @@ u8 SD_SendCommand(u8 cmd, u32 arg, u8 crc)
     //返回状态值
     return r1;
 }
-/************************************************************************																										
-** 函数功能: 向SD卡发送一个命令	 结束是不失能片选，还有后续数据传来
-** 入口参数: u8 cmd   命令 
-**			 u32 arg  命令参数
-**	    	 u8 crc   crc校验值	
-** 出口参数: SD卡返回的响应			
-** 备    注: 无
-************************************************************************/														  
 u8 SD_SendCommand_NoDeassert(u8 cmd, u32 arg, u8 crc)
 {
 	u8 Retry=0;	         
@@ -336,13 +315,6 @@ u8 SD_SendCommand_NoDeassert(u8 cmd, u32 arg, u8 crc)
     //返回响应值
     return r1;
 }
-/************************************************************************																										
-** 函数功能: 把SD卡设置到挂起模式
-** 入口参数: 无			    	 	
-** 出口参数: 0,成功设置
-			 1,设置失败	 
-** 备    注: 无
-************************************************************************/       
 u8 SD_Idle_Sta(void)
 {
 	u16 i;
@@ -365,67 +337,53 @@ u8 SD_Idle_Sta(void)
 	return 0;//成功	 						  
 }
 /************************************************************************																										
-** 函数功能: 初始化SD卡
-** 入口参数: 0：NO_ERR
-**			 1：TIME_OUT
-**			 99：NO_CARD
-** 出口参数: 无			
-** 备    注: 无
+** Description: Init SD card. make sd card to spi mode
+** Output: 0:NO_ERR
+**			 1:TIME_OUT
+**			 99:NO_CARD
 ************************************************************************/    																 
 u8 SD_Init(void)
 {								 
-    u8 r1;      // 存放SD卡的返回值
-    u16 retry;  // 用来进行超时计数
+    u8 r1;     //return value is card type by bit
+    u16 retry;  
     u8 buff[6];
-    //设置硬件上与SD卡相关联的控制引脚输出
-	//避免NRF24L01/W25X16等的影响   
+
+	//Init SSP0
 	ssp0_init();
  	SPIx_SetSpeed(SPI_SPEED_256);//设置到低速模式		 
 	SD_CS(1);	
-    if(SD_Idle_Sta()) return 1;//Card fail can not read	  
-    //-----------------SD卡复位到idle结束-----------------	 
-    //获取卡片的SD版本信息
+  if(SD_Idle_Sta()) return 1;//Card fail can not read	  
  	SD_CS(0);	
 	r1 = SD_SendCommand_NoDeassert(8, 0x1aa,0x87);	     
     //如果卡片版本信息是v1.0版本的，即r1=0x05，则进行以下初始化
     if(r1 == 0x05)
     {
-        //设置卡类型为SDV1.0，如果后面检测到为MMC卡，再修改为MMC
+        //Set sdtype to type v1
         SD_Type = SD_TYPE_V1;	   
-        //如果是V1.0卡，CMD8指令后没有后续数据
-        //片选置高，结束本次命令
+
         SD_CS(1);
-        //多发8个CLK，让SD结束后续操作
         ssp0_send(0xFF);	  
-        //-----------------SD卡、MMC卡初始化开始-----------------	 
-        //发卡初始化指令CMD55+ACMD41
-        // 如果有应答，说明是SD卡，且初始化完成
-        // 没有回应，说明是MMC卡，额外进行相应初始化
         retry = 0;
         do
         {
-            //先发CMD55，应返回0x01；否则出错
+					//Send command CMD55
             r1 = SD_SendCommand(CMD55, 0, 0);
-            if(r1 == 0XFF)return r1;//只要不是0xff,就接着发送	  
-            //得到正确响应后，发ACMD41，应得到返回值0x00，否则重试200次
-            r1 = SD_SendCommand(ACMD41, 0, 0);
+            if(r1 == 0XFF)return r1;//return error 
+          //Send commnad ACMD41  
+					r1 = SD_SendCommand(ACMD41, 0, 0);
             retry++;
         }while((r1!=0x00) && (retry<400));
-        // 判断是超时还是得到正确回应
-        // 若有回应：是SD卡；没有回应：是MMC卡	  
-        //----------MMC卡额外初始化操作开始------------
-        if(retry==400)
+				if(retry==400)
         {
             retry = 0;
-            //发送MMC卡初始化命令（没有测试）
             do
             {
                 r1 = SD_SendCommand(1,0,0);
                 retry++;
             }while((r1!=0x00)&& (retry<400));
             if(retry==400)return 1;   //MMC卡初始化超时		    
-            //写入卡类型
-            SD_Type = SD_TYPE_MMC;
+            //Set sdtype is MMC
+						SD_Type = SD_TYPE_MMC;
         }
         //----------MMC卡额外初始化操作结束------------	    
         //设置SPI为高速模式
@@ -490,13 +448,12 @@ u8 SD_Init(void)
     return r1;
 }	 					
 /************************************************************************																										
-** 函数功能: 从SD卡中读回指定长度的数据，放置在给定位置
-** 入口参数: u8 *data(存放读回数据的内存>len)
-**			 u16 len(数据长度）
-**			 u8 release(传输完成后是否释放总线CS置高 0：不释放 1：释放）
-** 出口参数: 0：NO_ERR
-**			 other：错误信息
-** 备    注: 无
+** Description: Receipt data from sd card
+** Input: u8 *data()
+**			 u16 len(len of data
+**			 u8 release(Data is return
+** Outpur: 0:NO_ERR
+**			 other: 
 ************************************************************************/ 	 														  
 u8 SD_ReceiveData(u8 *data, u16 len, u8 release)
 {
@@ -523,101 +480,10 @@ u8 SD_ReceiveData(u8 *data, u16 len, u8 release)
     return 0;
 }
 /************************************************************************																										
-** 函数功能: 获取SD卡的CID信息，包括制造商信息
-** 入口参数: u8 *cid_data(存放CID的内存，至少16Byte）
-** 出口参数: 0：NO_ERR
-**			 1：TIME_OUT
-**			 other：错误信息
-** 备    注: 无
-************************************************************************/														   
-u8 SD_GetCID(u8 *cid_data)
-{
-    u8 r1;	   
-    //发CMD10命令，读CID
-    r1 = SD_SendCommand(CMD10,0,0xFF);
-    if(r1 != 0x00)return r1;  //没返回正确应答，则退出，报错  
-    SD_ReceiveData(cid_data,16,RELEASE);//接收16个字节的数据	 
-    return 0;
-}	
-//获取SD卡的CSD信息，包括容量和速度信息
-//输入:u8 *cid_data(存放CID的内存，至少16Byte）	    
-//返回值:0：NO_ERR
-//       1：TIME_OUT
-//       other：错误信息														   
-u8 SD_GetCSD(u8 *csd_data)
-{
-    u8 r1;	 
-    r1=SD_SendCommand(CMD9,0,0xFF);//发CMD9命令，读CSD
-    if(r1)return r1;  //没返回正确应答，则退出，报错  
-    SD_ReceiveData(csd_data, 16, RELEASE);//接收16个字节的数据 
-    return 0;
-} 
-/************************************************************************																										
-** 函数功能: 获取SD卡的容量（字节）
-** 入口参数: u8 *data(存放读回数据的内存>len)
-**			 u16 len(数据长度）
-**			 u8 release(传输完成后是否释放总线CS置高 0：不释放 1：释放）
-** 出口参数: 0： 取容量出错
-**			 其他:SD卡的容量(字节)
-** 备    注: 无
-************************************************************************/     														  
-u32 SD_GetCapacity(void)
-{
-    u8 csd[16];
-    u32 Capacity;
-    u8 r1;
-    u16 i;
-	u16 temp;  					    
-	//取CSD信息，如果期间出错，返回0
-    if(SD_GetCSD(csd)!=0) return 0;	    
-    //如果为SDHC卡，按照下面方式计算
-    if((csd[0]&0xC0)==0x40)
-    {									  
-	    Capacity=((u32)csd[8])<<8;
-		Capacity+=(u32)csd[9]+1;	 
-        Capacity = (Capacity)*1024;//得到扇区数
-		Capacity*=512;//得到字节数			   
-    }
-    else
-    {		    
-    	i = csd[6]&0x03;
-    	i<<=8;
-    	i += csd[7];
-    	i<<=2;
-    	i += ((csd[8]&0xc0)>>6);
-        //C_SIZE_MULT
-    	r1 = csd[9]&0x03;
-    	r1<<=1;
-    	r1 += ((csd[10]&0x80)>>7);	 
-    	r1+=2;//BLOCKNR
-    	temp = 1;
-    	while(r1)
-    	{
-    		temp*=2;
-    		r1--;
-    	}
-    	Capacity = ((u32)(i+1))*((u32)temp);	 
-        // READ_BL_LEN
-    	i = csd[5]&0x0f;
-        //BLOCK_LEN
-    	temp = 1;
-    	while(i)
-    	{
-    		temp*=2;
-    		i--;
-    	}
-        //The final result
-    	Capacity *= (u32)temp;//字节为单位 	  
-    }
-    return (u32)Capacity;
-}
-/************************************************************************																										
-** 函数功能: 读SD卡的一个block
-** 入口参数: u32 sector 取地址（sector值，非物理地址）
-**			 u8 *buffer 数据存储地址（大小至少512byte）
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
+** Description: Read data from sdcard with single block
+** Input: u32 sector : Current sector where is used to read data
+**			 u8 *buffer : templ variable to store data
+** output: 
 ************************************************************************/      															  
 u8 SD_ReadSingleBlock(u32 sector, u8 *buffer)
 {
@@ -637,100 +503,13 @@ u8 SD_ReadSingleBlock(u32 sector, u8 *buffer)
 }
 
 
-////////////////////////////下面2个函数为USB读写所需要的/////////////////////////
-//定义SD卡的块大小	 				   
 #define BLOCK_SIZE 512 
-/************************************************************************																										
-** 函数功能: 写入MSD/SD数据 
-** 入口参数: pBuffer:数据存放区
-**			 ReadAddr:写入的首地址
-**			 NumByteToRead:要写入的字节数
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
-************************************************************************/
-u8 MSD_WriteBuffer(u8* pBuffer, u32 WriteAddr, u32 NumByteToWrite)
-{
-	u32 i,NbrOfBlock = 0, Offset = 0;
-	u32 sector;
-	u8 r1;
-   	NbrOfBlock = NumByteToWrite / BLOCK_SIZE;//得到要写入的块的数目	    
-    SD_CS(0);	  		   
-	while (NbrOfBlock--)//写入一个扇区
-	{
-		sector=WriteAddr+Offset;
-		if(SD_Type==SD_TYPE_V2HC)sector>>=9;//执行与普通操作相反的操作					  			 
-		r1=SD_SendCommand_NoDeassert(CMD24,sector,0xff);//写命令   
- 		if(r1)
-		{
-			SD_CS(1);
-			return 1;//应答不正确，直接返回 	   
-	    }
-	    ssp0_send(0xFE);//放起始令牌0xFE   
-	    //放一个sector的数据
-	    for(i=0;i<512;i++)ssp0_send(*pBuffer++);  
-	    //发2个Byte的dummy CRC
-	    ssp0_send(0xff);
-	    ssp0_send(0xff); 
- 		if(SD_WaitDataReady())//等待SD卡数据写入完成
-		{
-			SD_CS(1);
-			return 2;    
-		}
-		Offset += 512;	   
-	}	    
-    //写入完成，片选置1
-    SD_CS(1);
-    ssp0_send(0xff);	 
-    return 0;
-}
-/************************************************************************																										
-** 函数功能: 读取MSD/SD数据 
-** 入口参数: pBuffer:数据存放区
-**			 ReadAddr:写入的首地址
-**			 NumByteToRead:要读出的字节数
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
-************************************************************************/
-u8 MSD_ReadBuffer(u8* pBuffer, u32 ReadAddr, u32 NumByteToRead)
-{
-	u32 NbrOfBlock=0,Offset=0;
-	u32 sector=0;
-	u8 r1=0;   	 
-  	NbrOfBlock=NumByteToRead/BLOCK_SIZE;	  
-    SD_CS(0);
-	while (NbrOfBlock --)
-	{	
-		sector=ReadAddr+Offset;
-		if(SD_Type==SD_TYPE_V2HC)sector>>=9;//执行与普通操作相反的操作					  			 
-		r1=SD_SendCommand_NoDeassert(CMD17,sector,0xff);//读命令	 		    
-		if(r1)//命令发送错误
-		{
-    		SD_CS(1);
-			return r1;
-		}	   							  
-		r1=SD_ReceiveData(pBuffer,512,RELEASE);		 
-		if(r1)//读数错误
-		{
-    		SD_CS(1);
-			return r1;
-		}
-		pBuffer+=512;	 					    
-	  	Offset+=512;				 	 
-	}	 	 
-    SD_CS(1);
-    ssp0_send(0xff);	 
-    return 0;
-}
 //////////////////////////////////////////////////////////////////////////
 /************************************************************************																										
-** 函数功能: 写入SD卡的一个block(未实际测试过) 
-** 入口参数: u32 sector 扇区地址（sector值，非物理地址）
-**			 u8 *buffer 数据存储地址（大小至少512byte）
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
+** Description: Write data to sdcard with single block  
+** Input: u32 sector :Current sector to write data to sdcard
+**			 u8 *data : is data from buffer
+** Output: 
 ************************************************************************/															  
 u8 SD_WriteSingleBlock(u32 sector, const u8 *data)
 {
@@ -795,13 +574,11 @@ u8 SD_WriteSingleBlock(u32 sector, const u8 *data)
     return 0;
 }
 /************************************************************************																										
-** 函数功能: 读SD卡的多个block(实际测试过) 
-** 入口参数: 输入:u32 sector 扇区地址（sector值，非物理地址）
-**			 u8 *buffer 数据存储地址（大小至少512byte）
-**			 u8 count 连续读count个block
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
+** Decription: Read multi Block directly from sdcard 
+** Input: :u32 sector Current sector where to read data
+**			 u8 *buffer buffer (ram variable) is used to templ store data
+**			 u8 count count of block
+** Output: 
 ************************************************************************/															  
 u8 SD_ReadMultiBlock(u32 sector, u8 *buffer, u8 count)
 {
@@ -827,13 +604,11 @@ u8 SD_ReadMultiBlock(u32 sector, u8 *buffer, u8 count)
     else return 0;	 
 }		
 /************************************************************************																										
-** 函数功能: 写入SD卡的N个block(未实际测试过) 
-** 入口参数: u32 sector 扇区地址（sector值，非物理地址）
-**			 u8 *buffer 数据存储地址（大小至少512byte）
-**			 u8 count 写入的block数目
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
+** Description: Write multi block to sdcard 
+** Input: u32 sector: current sector to strore data in sdcard
+**			 u8 *buffer data to write to sdcard 
+**			 u8 count Count of block
+** Output: 
 ************************************************************************/															   
 u8 SD_WriteMultiBlock(u32 sector, const u8 *data, u8 count)
 {
@@ -891,35 +666,3 @@ u8 SD_WriteMultiBlock(u32 sector, const u8 *data, u8 count)
     ssp0_send(0xff);  
     return count;   //返回count值，如果写完则count=0，否则count=1
 }
-/************************************************************************																										
-** 函数功能: 在指定扇区,从offset开始读出bytes个字节 
-** 入口参数: u32 sector 扇区地址（sector值，非物理地址）
-**			 u8 *buf     数据存储地址（大小<=512byte）
-**			 u16 offset  在扇区里面的偏移量
-**			 u16 bytes   要读出的字节数
-** 出口参数: 0： 成功
-**			 other：失败
-** 备    注: 无
-************************************************************************/															   
-u8 SD_Read_Bytes(unsigned long address,unsigned char *buf,unsigned int offset,unsigned int bytes)
-{
-    u8 r1;u16 i=0;  
-    r1=SD_SendCommand(CMD17,address<<9,0);//发送读扇区命令      
-    if(r1)return r1;  //应答不正确，直接返回
-	SD_CS(0);//选中SD卡
-	if(SD_GetResponse(0xFE))//等待SD卡发回数据起始令牌0xFE
-	{
-		SD_CS(1); //关闭SD卡
-		return 1;//读取失败
-	}	 
-	for(i=0;i<offset;i++)ssp0_send(0xff);//跳过offset位 
-    for(;i<offset+bytes;i++)*buf++=ssp0_send(0xff);//读取有用数据	
-    for(;i<512;i++) ssp0_send(0xff); 	 //读出剩余字节
-    ssp0_send(0xff);//发送伪CRC码
-    ssp0_send(0xff);  
-    SD_CS(1);//关闭SD卡
-	return 0;
-}
-
-
-
