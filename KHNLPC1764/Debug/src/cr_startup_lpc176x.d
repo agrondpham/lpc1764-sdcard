d37b28b6b96747c6d92ab78c925c19d15edee81b
pthelong@gmail.com <pthelong@gmail.com@5c9c312e-5ef8-758d-1564-8314ce06484d>
2014-04-08 07:34:47 +0000

    remove folder debug

--- KHNLPC1764/Debug/src/cr_startup_lpc176x.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc176x.d: ../src/cr_startup_lpc176x.c \
- C:\Users\Long\Documents\LPCXpresso_7.0.0_92\workspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h
-
-C:\Users\Long\Documents\LPCXpresso_7.0.0_92\workspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
