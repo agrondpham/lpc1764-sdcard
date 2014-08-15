3951cd939aae8276d6ce2cd11ec2667be140af8d
pthelong@gmail.com <pthelong@gmail.com@5c9c312e-5ef8-758d-1564-8314ce06484d>
2014-08-15 04:19:43 +0000

    This branch LCP1764 can get data from server

--- KHNLPC1764/src/IAP/khn_flash.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include "LPC17xx.h"
-
-
-int writeFlash();
-int clearFlash();
-int readFlash();
--- KHNLPC1764/src/Tcp/tcp.h
+++
@@ -1,6 +0,0 @@
-#include "LPC17xx.h"
-#include "../common.h"
-#include "../uart/uart.h"
-#include "../gsm/gsm.h"
-#define cstt_error  0
-#define ciicr_error 1
